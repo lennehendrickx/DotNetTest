fe7dd5a4de1340e1da48dadf1ab4c58dd13d2fd2
Lenne Hendrickx <lennehendrickx@gmail.com>
2015-12-06 23:17:59 +0100

    Use language features that do not exist in Java

--- DotNetTest/DotNetTest/Sandbox.cs
+++ DotNetTest/DotNetTest/Sandbox.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DotNetTest
 {
-    class Program
+    class Sandbox
     {
         static void Main(string[] args)
         {
--- DotNetTest/DotNetUnitTest/Properties/AssemblyInfo.cs
+++ DotNetTest/DotNetUnitTest/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DotNetUnitTeste")]
+[assembly: AssemblyTitle("DotNetUnitTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DotNetUnitTeste")]
+[assembly: AssemblyProduct("DotNetUnitTest")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
